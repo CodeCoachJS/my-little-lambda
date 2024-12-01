dc4f5e0222482caf2b0e7cc7e1b7487ebee71a6a
Brian Jenney <brianjenney83@gmail.com>
2024-12-01 08:56:36 -0800

    parse body

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ const Jimp = require('jimp');
 const s3 = new AWS.S3();
 
 const handler = async (event) => {
+	const parsedBody = JSON.parse(event.body);
 	const bucketName = 'parsity';
-	const fileName = 'transformed-image.jpg';
+	const fileName = `${parsedBody.fileName}.jpg;`;
 
 	try {
-		const base64String = JSON.parse(event.body).image;
+		const base64String = parsedBody.image;
 		const buffer = Buffer.from(base64String, 'base64');
 
 		const image = await Jimp.read(buffer);
